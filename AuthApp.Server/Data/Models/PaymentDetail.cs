af8be3b2c4bd8225c021791a40bd060c342941c6
Barna Gergely <h144988@stud.u-szeged.hu>
2024-12-25 16:46:34 +0100

    Create Payment model

---
+++ AuthApp.Server/Data/Models/PaymentDetail.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthApp.Server.Data.Models;
+
+public record PaymentDetail
+{
+    [Key]
+    public int PaymentDetailId { get; set; }
+    public string CardOwnerName { get; set; } = string.Empty; // TODO: Specify column type The method used in the tutorial is suspicious: https://youtu.be/OZGdKYzUYvU?si=Q95b_NtbBGUTJo5L&t=294
+    public string CardNumber { get; set; } = string.Empty;
+    public string ExpirationDate { get; set; } = string.Empty;
+    public string SecurityCode { get; set; } = string.Empty;
+}
